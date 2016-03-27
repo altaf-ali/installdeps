c8aa16565688de129839d79fde0d5f0cc5a6d994
Altaf Ali <altaf@fastmail.com>
2016-03-28 00:37:31 +0100

    bug fixes

--- R/installdeps.R
+++ R/installdeps.R
@@ -1,14 +1,11 @@
-library(stringr)
-library(rstudioapi)
-
 # install all dependencies in the current script
 
 install_dependencies <- function() {
   installed_packages <- row.names(installed.packages())
-  doc <- getActiveDocumentContext()
+  doc <- rstudioapi::getActiveDocumentContext()
   for (s in doc$contents) {
-    m <- str_match(s, "library\\((.*)\\)")
-    package_name <- str_trim(m[1,2])
+    m <- stringr::str_match(s, "library\\((.*)\\)")
+    package_name <- stringr::str_trim(m[1,2])
     if (!is.na(package_name)) {
       if (!(package_name %in% installed_packages)) {
         install.packages(package_name)
