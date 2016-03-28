88fc1491a7a760317d75ea5259faed64dc179721
Altaf Ali <altaf@fastmail.com>
2016-03-28 01:29:44 +0100

    error handling

--- R/installdeps.R
+++ R/installdeps.R
@@ -1,15 +1,21 @@
 # install all dependencies in the current script
 
 install_dependencies <- function() {
-  installed_packages <- row.names(installed.packages())
-  doc <- rstudioapi::getActiveDocumentContext()
-  for (s in doc$contents) {
-    m <- stringr::str_match(s, "library\\((.*)\\)")
-    package_name <- stringr::str_trim(m[1,2])
-    if (!is.na(package_name)) {
-      if (!(package_name %in% installed_packages)) {
-        install.packages(package_name)
-      }
+  tryCatch({
+    doc <- rstudioapi::getActiveDocumentContext()
+    installed_packages <- row.names(installed.packages())
+    for (s in doc$contents) {
+      m <- stringr::str_match(s, "library\\((.*)\\)")
+      package_name <- stringr::str_trim(m[1,2])
+      if (is.na(package_name) || (package_name %in% installed_packages))
+        next
+      install.packages(package_name)
     }
-  }
+  }, error = function(e) {
+    # need error handling or a better message?
+    # message(e)
+  })
 }
+
+
+
