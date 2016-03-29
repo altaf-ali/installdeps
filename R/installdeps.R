995e21678a075948cb50bdbd9cc855bf7b63f945
Altaf Ali <altaf@fastmail.com>
2016-03-29 01:44:50 +0100

    use common code

--- R/installdeps.R
+++ R/installdeps.R
@@ -1,23 +1,5 @@
-# check all dependencies in the current script
-check_dependencies <- function() {
-  tryCatch({
-    doc <- rstudioapi::getActiveDocumentContext()
-    installed_packages <- row.names(installed.packages())
-    for (s in doc$contents) {
-      m <- stringr::str_match(s, "library\\((.*)\\)")
-      package_name <- stringr::str_trim(m[1,2])
-      if (is.na(package_name) || (package_name %in% installed_packages))
-        next
-      message(sprintf("Required package '%s' is missing", package_name))
-    }
-  }, error = function(e) {
-    # need error handling or a better message?
-    # message(e)
-  })
-}
-
 # install all dependencies in the current script
-install_dependencies <- function() {
+install_dependencies <- function(check = FALSE) {
   tryCatch({
     doc <- rstudioapi::getActiveDocumentContext()
     installed_packages <- row.names(installed.packages())
@@ -26,8 +8,16 @@ install_dependencies <- function() {
       package_name <- stringr::str_trim(m[1,2])
       if (is.na(package_name) || (package_name %in% installed_packages))
         next
-      message(sprintf("Installing package '%s'", package_name))
-      install.packages(package_name)
+
+      if (check)
+        msg <- sprintf("Required package '%s' is missing", package_name)
+      else
+        msg <- sprintf("Installing package '%s'", package_name)
+
+      message(msg)
+
+      if (!check)
+        install.packages(package_name)
     }
   }, error = function(e) {
     # need error handling or a better message?
@@ -35,6 +25,6 @@ install_dependencies <- function() {
   })
 }
 
-
-
+# check all dependencies in the current script
+check_dependencies <- function() { install_dependencies(check = TRUE) }
 
