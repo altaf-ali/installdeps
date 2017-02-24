e167d030b3960f3c10fdddb00aaa49c45676fa5d
Altaf Ali <altaf@fastmail.com>
2017-02-24 02:19:24 +0000

    updating minor changes

--- R/installdeps.R
+++ R/installdeps.R
@@ -4,8 +4,8 @@ install_dependencies <- function(check = FALSE) {
     doc <- rstudioapi::getActiveDocumentContext()
     installed_packages <- row.names(installed.packages())
     for (s in doc$contents) {
-      m <- stringr::str_match(s, "^\\s*?library\\((.*)\\)\\s*$")
-      package_name <- stringr::str_trim(m[1,2])
+      m <- stringr::str_match(s, "^\\s*?(library|require)\\((.*)\\)\\s*$")
+      package_name <- stringr::str_trim(m[1,3])
       if (is.na(package_name) || (package_name %in% installed_packages))
         next
 
@@ -28,3 +28,32 @@ install_dependencies <- function(check = FALSE) {
 # check all dependencies in the current script
 check_dependencies <- function() { install_dependencies(check = TRUE) }
 
+# format_code
+format_code <- function() {
+  #s = "  > go.wish <- function(reps, Sig, df){"
+
+  tryCatch({
+    doc <- rstudioapi::getActiveDocumentContext()
+    row <- 1
+    for (s in doc$contents) {
+      pattern <- "^(\\s*[>\\+]).*"
+      if (grepl(pattern, s)) {
+        m <- stringr::str_match(s, pattern)
+        column <- 1
+        target_length <- nchar(m[2])
+
+        start <- rstudioapi::document_position(row, column)
+        end <- rstudioapi::document_position(row, column + target_length)
+
+        replacement_text <- paste0(rep(' ', target_length), collapse = '')
+        message("Modifying row ", row, " col ", column)
+        rstudioapi::modifyRange(rstudioapi::document_range(start, end), replacement_text)
+      }
+
+      row <- row + 1
+    }
+  }, error = function(e) {
+    # need error handling or a better message?
+    message(e)
+  })
+}
